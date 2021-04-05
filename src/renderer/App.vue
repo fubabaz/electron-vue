93d0b48ac59fc0bf3beec617205bb12f32fb73d8
zeroradish <zeroradish@gmail.com>
2021-04-05 20:52:20 +0900

    Hi! fubabaz

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'incubator-electron-sdq-launcher'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
